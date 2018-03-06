486baa1b246dc456560adcba522decefe1386fd8
Andreas Tscheinig <GustavAT@users.noreply.github.com>
2018-03-06 14:57:00 +0100

    Fixed layout issues

--- app/src/main/java/at/cywl/palindromchecker/PalindromeActivity.java
+++ app/src/main/java/at/cywl/palindromchecker/PalindromeActivity.java
@@ -1,27 +1,28 @@
 package at.cywl.palindromchecker;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class Palindrome extends AppCompatActivity {
+public class PalindromeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palindrome);
 
-        Button buttonCheck = (Button) findViewById(R.id.buttonCheck);
+        Button buttonCheck = findViewById(R.id.buttonCheck);
 
         buttonCheck.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TextView sourceField = (TextView) findViewById(R.id.text_palindrome);
+                TextView sourceField = findViewById(R.id.text_palindrome);
                 String text = sourceField.getText().toString();
-                String status = IsPalindrome(text);
+                String status = IsPalindrome(view.getContext(), text);
 
                 ((TextView) findViewById(R.id.textview_status)).setText(status);
             }
@@ -33,12 +34,14 @@ public class Palindrome extends AppCompatActivity {
      * @param target string to check
      * @return 0 if given string is a palindrome, 1 if string is null and 2 if the strins length is < 5
      */
-    protected static String IsPalindrome(String target) {
-        // todo: move to resources
-        if (target == null) return "Given string is null!";
+    protected static String IsPalindrome(Context ctx, String target) {
+        if (target == null) return ctx.getString(R.string.text_target_is_null);
 
-        if (target.length() < 5) return "Given string must have at least 5 characters!";
+        target = target.trim();
+        if (target.length() < 5) return ctx.getString(R.string.text_string_too_short);
         boolean result = target.equals(new StringBuilder(target).reverse().toString());
-        return result ? "Palindrome" : "Not a palindrome";
+        return result
+                ? ctx.getString(R.string.text_palindrome)
+                : ctx.getString(R.string.text_not_palindrome);
     }
 }
